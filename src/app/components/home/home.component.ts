712d91b3f58f5f3057a7ee9f97dd5382e47b4be3
He Yang <heyang0012856@gmail.com>
2019-06-06 17:22:37 +0800

    add ng class style and model

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public flag:boolean=true;
+  public order:number=2;
+  public attr:string='orange';
+
+  public today:any=new Date();
+  public keywords:string='这是默认的值';
+  
+  constructor() {
+
+    console.log(this.today);
+   }
 
   ngOnInit() {
+  }
+  run(){
+    alert('执行方法');
+  }
+  changekeywords(){
+    this.keywords="im the changed value";
+  }
+  getkeywords(){
+    alert(this.keywords);
+
   }
 
 }
--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -13,10 +13,29 @@ export class NewsComponent implements OnInit {
     age:20,
   }
   public message:any;
+  public content="<h2>this is a h2 tag</h2>"
   constructor() { 
     this.message="this is initialization for message";
 
   }
+  // 定义数组
+  public arr=['111','222','333'];
+  //推荐写法
+  public list:any=['this is one', 22222, 'this is two'];
+
+  public items:Array<string>=['wo shi di yi ge', 'wo shi di er ge'];
+
+  public userlist:any=[
+    {
+      username:"zhangsan",
+      age:20
+    },
+    {
+      username:"lisi",
+      age:30
+    }
+    
+  ]
 
   ngOnInit() {
   }
